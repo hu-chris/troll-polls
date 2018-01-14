69ab218b43f52f5eab05c7af4c20673a260a6aa3
Hu-chris <birdbrainguy@gmail.com>
2018-01-13 18:02:47 -0700

    adsf

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ app = express();
 
 var port = process.env.PORT || 8080
 
-app.set('view engine', 'ejs');
-
 app.use(express.static(__dirname + 'public'));
 
 app.get('/', function(req, res) {
-    res.render('view/index.html');
+    res.render('views/index.html');
 })
 
 app.listen(port, function(){
