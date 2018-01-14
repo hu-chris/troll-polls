aae1f2560a56a1642e38bc7ae3d4f682e015e2bd
Hu-chris <birdbrainguy@gmail.com>
2018-01-13 18:03:40 -0700

    asdfas

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 8080
 app.use(express.static(__dirname + 'public'));
 
 app.get('/', function(req, res) {
-    res.sendFile('views/index.html');
+    res.end('jesus fucking christ this is stupid');
 })
 
 app.listen(port, function(){
